cfc5fd91b0255225c811182e3e800074335de0e0
nebrassy <nebras30@gmail.com>
2024-09-21 16:20:00 +0200

    recovery: support AIDL GateKeeper 2/2
    Change-Id: I68fec3eb89cf0996605135eb8b8743b6c90e4cf2

--- Android.mk
+++ Android.mk
@@ -341,6 +341,8 @@ ifeq ($(TW_INCLUDE_CRYPTO), true)
 	android.security.authorization-ndk_platform \
         android.hardware.oemlock@1.0 libf2fs_sparseblock libbinder libbinder_ndk \
         libandroidicu.recovery \
+        lib_android_keymaster_keymint_utils \
+        android.hardware.gatekeeper-V1-ndk \
         android.hardware.gatekeeper@1.0 \
         android.hardware.weaver@1.0 \
         android.frameworks.stats@1.0 \
--- libtar/Android.mk
+++ libtar/Android.mk
@@ -18,6 +18,8 @@ ifeq ($(TW_INCLUDE_CRYPTO_FBE), true)
     LOCAL_SHARED_LIBRARIES +=  \
         android.hardware.boot@1.0 \
         android.hardware.confirmationui@1.0 \
+        lib_android_keymaster_keymint_utils \
+        android.hardware.gatekeeper-V1-ndk \
         android.hardware.gatekeeper@1.0 \
         android.hardware.keymaster@3.0 \
         android.hardware.keymaster@4.0 \
